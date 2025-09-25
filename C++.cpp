7f509645c8b56a6500e13bd21ca6fd85f2022c4f
uzairnasir001 <uzairnasir477@gmail.com>
2025-09-25 23:21:18 +0500

    C++ if else programs

---
+++ C++.cpp
@@ -0,0 +1,283 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // if else
+    // Program 5.1
+
+    // int marks;
+    // cout<<"Enter your number?"<<endl;
+    // cin>>marks;
+    // if (marks>=40)
+    // {
+    //     cout<<"Congraulation, You have passed."<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"You are fail."<<endl;
+    // }
+
+    // Program 5.2
+
+    // int a;
+    // int b;
+
+    // cout<<"Enter the first number "<<endl;
+    // cin>>a;
+    // cout<<"Enter the second number "<<endl;
+    // cin>>b;
+    // if (a==b)
+    // {
+    //     cout<<"The numbers are equal. "<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"The numbers are not equal."<<endl;
+    // }
+
+    // Program 5.3
+
+    // int a;
+    // int b;
+    // cout<<"Enter the first number:"<<endl;
+    // cin>>a;
+    // cout<<"Enter the second number:"<<endl;
+    // cin>>b;
+    // if (a*a==b)
+    // {
+    //     cout<<"The second number is the square of first number."<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"The second number is not the square of first number."<<endl;
+    // }
+
+    // Program 5.4
+
+    // int sub1;
+    // int sub2;
+    // int sub3;
+    // float avg;
+     
+    // cout<<"Enter the first subject marks:"<<endl;
+    // cin>>sub1;
+    // cout<<"Enter the second subject marks:"<<endl;
+    // cin>>sub2;
+    // cout<<"Enter the third subject marks:"<<endl;
+    // cin>>sub3;
+    // avg=sub1+sub2+sub3/3;
+    // if (avg>80)
+    // {
+    //     cout<<"You are above standard."<<endl;
+    //     cout<<"Admission Granted."<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"Admission not Granted."<<endl;
+    // }
+    
+    // Program 5.5
+    
+    // int a,b,c,max;
+    // cout<<"Enter the first number:"<<endl;
+    // cin>>a;
+    // cout<<"Enter the second number:"<<endl;
+    // cin>>b;
+    // cout<<"Enter the third number:"<<endl;
+    // cin>>c;
+    // max=a;
+    // if (b>max)
+    // {
+    //     max=b;
+    // }
+    // if (c>max)
+    // {
+    //     max=c;
+    // }
+    // cout<<"The maximum number is "<<max<<endl;
+ 
+    // Program 5.6
+    
+    // int n;
+    // cout<<"Enter a number:"<<endl;
+    // cin>>n;
+    // if (n>0)
+    // {
+    //     cout<<"The number is positive."<<endl;
+    // }
+    // if (n<0)
+    // {
+    //    cout<<"The number is negative."<<endl;
+    // }
+    // if (n==0)
+    // {
+    //     cout<<"The number is zero."<<endl;
+    // }
+    
+    // Program 5.7
+ 
+    // int a,b,c,d,e,min,max;
+    // cout<<"Enter the five integers:"<<endl;
+    // cin>>a>>b>>c>>d>>e;
+    // max=a;
+    // min=a;
+    // if (b>max)
+    // {
+    //     max=b;
+    // }
+    // if (c>max)
+    // {
+    //     max=c;
+    // }
+    // if (d>max)
+    // {
+    //     max=d;
+    // }
+    // if (e>max)
+    // {
+    //     max=e;
+    // }
+    // if (b<min)
+    // {
+    //     min=b;
+    // }
+    // if (c<min)
+    // {
+    //     min=c;
+    // }
+    // if (d<min)
+    // {
+    //     min=d;
+    // }
+    // if (e<min)
+    // {
+    //     min=e;
+    // }
+    // cout<<"The maximum number is "<<max<<endl;
+    // cout<<"The minimum number is "<<min<<endl;
+     
+
+    // Program 5.8
+    // int n;
+    // cout<<"Enter a number:"<<endl;
+    // cin>>n;
+    // if (n%2==0)
+    // {
+    //     cout<<"The number is even."<<endl;
+    // }
+    // else
+    // {
+    //    cout<<"The number is odd."<<endl;
+    // }
+    
+    // Program 5.9
+    // int year;
+    // cout<<"Enter a year:"<<endl;
+    // cin>>year;
+    // if (year%4==0)
+    // {
+    //     cout<<year<<" is a leap year."<<endl;
+    // }
+    // else
+    // {
+    //     cout<<year<<" is not a leap year."<<endl;
+    // }
+      
+    // Program 5.10
+    // float salary, bonous, total_salary;
+    // int grade;
+    // cout<<"Enter the salary:"<<endl;
+    // cin>>salary;
+    // cout<<"Enter the Grade:"<<endl;
+    // cin>>grade;
+    // if (grade>15)
+    // {
+    //     bonous=salary*0.5;
+    // }
+    // else
+    // {
+    //     bonous=salary*0.25;
+    // }
+    // total_salary=salary+bonous;
+    // cout<<"The Total salary is "<<total_salary<<endl;
+
+    // Program 5.11
+
+    // int a,b;
+    // cout<<"Enter the first number:"<<endl;
+    // cin>>a;
+    // cout<<"Enter the second number:"<<endl;
+    // cin>>b;
+    // if (a%b==0)
+    // {
+    //     cout<<"The first number is the multiple of second number."<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"The first number is not the multiple of second number."<<endl;
+    // }
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
